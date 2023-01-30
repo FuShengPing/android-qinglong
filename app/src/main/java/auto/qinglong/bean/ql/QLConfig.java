2c46e76612f2aecab2396cebf1cdb8a0204b87db
FuShengPing <103162025+FuShengPing@users.noreply.github.com>
2023-01-30 15:42:59 +0800

    优化

---
+++ app/src/main/java/auto/qinglong/bean/ql/QLConfig.java
@@ -0,0 +1,13 @@
+package auto.qinglong.bean.ql;
+
+public class QLConfig {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
