baac1b6dc9f4f9c2da1d1dfcdee7b0cd1755f446
FuShengPing <103162025+FuShengPing@users.noreply.github.com>
2023-01-30 15:44:46 +0800

    优化

--- app/src/main/java/auto/qinglong/MyApplication.java
+++ app/src/main/java/auto/qinglong/MyApplication.java
@@ -1,11 +1,7 @@
 package auto.qinglong;
 
-import android.Manifest;
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.PackageManager;
-
-import androidx.core.content.ContextCompat;
 
 public class MyApplication extends Application {
     private static Context context;
@@ -21,18 +17,6 @@ public class MyApplication extends Application {
      */
     public static Context getContext() {
         return context;
-    }
-
-    /**
-     * 检测权限是否完全
-     *
-     * @return the boolean
-     */
-    public static boolean checkSelfPermission() {
-        //存储权限
-        int f1 = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int f2 = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE);
-        return f1 == PackageManager.PERMISSION_GRANTED && f2 == PackageManager.PERMISSION_GRANTED;
     }
 
 }
