973248a16da28c3733bb69e95efc7a3b5fc42118
FuShengPing <103162025+FuShengPing@users.noreply.github.com>
2023-07-25 20:06:15 +0800

    update

--- proxy/src/main/java/auto/ssh/ui/activity/ConfigActivity.java
+++ proxy/src/main/java/auto/ssh/ui/activity/ConfigActivity.java
@@ -1,15 +1,26 @@
 package auto.ssh.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import auto.ssh.R;
 
 public class ConfigActivity extends BaseActivity {
+    private View uiExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_config);
+
+        uiExit = findViewById(R.id.exit);
+
+        init();
+    }
+
+    private void init(){
+        // 退出
+        uiExit.setOnClickListener(v -> finish());
     }
 }
